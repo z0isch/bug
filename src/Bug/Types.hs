97ca136d24a85a006182d8a5f9525560b24c263b
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-01-18 10:22:27 -0500

    Can eat implemented

--- src/Bug/Types.hs
+++ src/Bug/Types.hs
@@ -10,10 +10,7 @@ import Control.Lens
 import Data.Vector.Unboxed ((!), Vector)
 import qualified Data.Vector.Unboxed as V
 import Data.Word
-import Data.List
 import Linear.V2
-import Linear.V3
-import Linear.Matrix
 import Linear.Vector
 import Data.HashSet (HashSet)
 import qualified Data.HashSet as H
@@ -69,41 +66,46 @@ mkGrid s = Grid s $ V.generate (sideLength*sideLength) mkPiece
 bugs :: Grid -> (HashSet Bug, HashSet Bug)
 bugs (Grid s g) = V.ifoldl' mkPs (mempty,mempty) g
     where
-        smash = foldl' f mempty
-        f ps p = maybe (H.insert p ps) (\b' -> H.insert (H.union b' p) $ H.delete b' ps) b
-            where b = headMay $ toList $ H.filter (getAny . foldMap (Any . (\p' -> getAny $ foldMap (Any . isNeighbor p') p))) ps
         mkPs p i = \case
-            1 -> over _1 (smash . H.insert (H.singleton (frmIdx s i))) p
-            2 -> over _2 (smash . H.insert (H.singleton (frmIdx s i))) p
+            1 -> over _1 (insertIt i) p
+            2 -> over _2 (insertIt i) p
             _ -> p
+        insertIt i = smash . H.insert (H.singleton (frmIdx s i))
+        smash = foldl' addEl mempty
+        addEl ps p = maybe (H.insert p ps) (\b' -> H.insert (H.union b' p) $ H.delete b' ps) b
+            where b = headMay $ toList $ H.filter (isAdjacentBug p) ps
+        
+canEat :: Bug -> Bug -> Bool
+canEat b1 b2 = isAdjacentBug b1 b2 && isIsomorphicBug b1 b2
 
-isSameBug :: Bug -> Bug -> Bool
-isSameBug b1 b2 
-    | sameSize = H.size b1 == 1 || H.size b1 == 2 || cb1 `elem` rotated ++ reflected
+isAdjacentBug :: Bug -> Bug -> Bool
+isAdjacentBug b1 = getAny . foldMap (\p1 -> foldMap (Any . isNeighbor p1) b1)
+
+isIsomorphicBug :: Bug -> Bug -> Bool
+isIsomorphicBug b1 b2 
+    | sameSize = H.size b1 == 1 || H.size b1 == 2 || cannonicalBug b1 `elem` perms b2
     | otherwise = False
     where
         sameSize = H.size b1 == H.size b2
-        cb1 = cannonicalBug b1
-        rotated = (map cannonicalBug . rotations) b2
-        reflected = (map (cannonicalBug . reflect) . rotations) b2
+
+perms :: Bug -> [Bug]
+perms b = map cannonicalBug $ (concatMap (\rot ->  [rot,reflect rot]) . rotations) b
 
 rotations :: Bug -> [Bug]
-rotations b = [rotate60 b, rotate60 $ rotate60 b,rotate60 $ rotate60 $ rotate60 b,rotate60 $ rotate60 $ rotate60 $ rotate60 b,rotate60 $ rotate60 $ rotate60 $ rotate60 $ rotate60 b]
+rotations = take 6 . iterate (rotate60 (V2 0 0))
 
 reflect :: Bug -> Bug
 reflect = H.map f
     where f (V2 x z) = V2 x' z'
             where y = -z - x
-                  y' = y
-                  x' = y'
+                  x' = y
                   z' = z
+
 centerPoint :: Bug -> BPoint
 centerPoint = minimumBy (\(V2 q r) (V2 q' r') -> if q > q' then GT else if q < q' then LT else compare r r')
 
-rotate60 :: Bug -> Bug
-rotate60 b = H.map (\p -> let (V2 x z) = p ^-^ c in c ^+^ V2 (-z) (x + z)) b
-    where 
-        c = centerPoint b
+rotate60 :: BPoint -> Bug -> Bug
+rotate60 c = H.map (\p -> let (V2 x z) = p ^-^ c in c ^+^ V2 (-z) (x + z))
 
 cannonicalBug :: Bug -> Bug
 cannonicalBug b = H.map (^+^ negated (centerPoint b)) b
@@ -113,5 +115,9 @@ setToPlayer pl pts g@(Grid s _) = over grid (`V.update` V.fromList (map (\x-> (i
 
 testGrid :: Grid
 testGrid = setToPlayer 2 [V2 0 (-1),V2 (-2) 0,V2 1 (-2), V2 (-1) 0,V2 0 (-2), V2 (-2) 1] 
-         $ setToPlayer 1 [V2 (-2) 2, V2 (-1) 1,V2 0 1, V2 2 (-1),V2 1 (-1), V2 0 0] 
+         $ setToPlayer 1 [V2 (-1) 1,V2 0 1, V2 2 (-1),V2 1 (-1), V2 0 0, V2 2 (-2)] 
          $ mkGrid 3
+testGrid2 :: Grid
+testGrid2 = setToPlayer 2 [V2 0 (-1),V2 (-2) 0,V2 1 (-2), V2 (-1) 0,V2 0 (-2), V2 (-2) 1] 
+        $ setToPlayer 1 [V2 (-2) 2, V2 (-1) 1,V2 0 1, V2 2 (-1),V2 1 (-1), V2 0 0] 
+        $ mkGrid 3
